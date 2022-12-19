56ae254cf065798f769061dc05eefefc6da4072d
Praise25 <anenepraise1@gmail.com>
2022-12-19 20:17:17 +0100

    refactor: move sensitive data to .env file

--- models/db-connection.js
+++ models/db-connection.js
@@ -1,13 +1,13 @@
 const { Sequelize, QueryTypes } = require("sequelize");
 
-const DB_NAME = "bincom_test";
+const DB_NAME = process.env.DB_NAME;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const selectQueryConfig = { type: QueryTypes.SELECT };
 const insertQueryConfig = { type: QueryTypes.INSERT };
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: "localhost",
+  host: process.env.DB_HOST,
   dialect: "mysql",
 });
 

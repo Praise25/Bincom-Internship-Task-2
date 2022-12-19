1f0106c47d41f05dceb65c951c36a78e5b680798
Praise25 <anenepraise1@gmail.com>
2022-12-19 15:57:30 +0100

    feat: add route for POST request to /polling-units

--- routes/polling-units.js
+++ routes/polling-units.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 const pollingUnit = require("../controllers/polling-units");
 
-router.get("/", pollingUnit.getPollingUnits);
+router.route("/").get(pollingUnit.getPollingUnits).post(pollingUnit.getPollingUnitResult);
 
 router
   .route("/new")
   .get(pollingUnit.getNewPollingUnitForm)
   .post(pollingUnit.createNewPollingUnit);
 
-router.get("/:id/result", pollingUnit.getPollingUnitResult);
+router.get("/:id/result", pollingUnit.showPollingUnitResult);
 
 module.exports = router;
